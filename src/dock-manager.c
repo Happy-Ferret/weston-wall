5f76682fcb4fb98dfad1f4b84ee7d9c6f2a8ff11
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-01-17 18:53:11 +0100

    dock-manager: Update for new Weston module API
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/dock-manager.c
+++ src/dock-manager.c
@@ -337,7 +337,7 @@ _weston_dock_manager_output_destroyed(struct wl_listener *listener, void *data)
 }
 
 WW_EXPORT int
-module_init(struct weston_compositor *compositor, int *argc, char *argv[])
+wet_module_init(struct weston_compositor *compositor, int *argc, char *argv[])
 {
     struct weston_dock_manager *self;
 
